f7e9d9eb2d027ab7b82536f2e85a72bb2ae6b141
biaafarah <rm97865@fiap.com.br>
2023-09-13 15:12:09 -0300

    trabalho (quase) final

--- Sprint.py
+++ Sprint.py
@@ -1,18 +1,12 @@
-listaEstabelecimentos = []
-listaUsuarios = []
-listaPostsUsuario = []
-listaPostsEstabelecimento = []
-listaReviews = []
-listaCupons = []
-listaNotas = []
+listaEstabelecimentos = [{'email':'oiii', 'estabelecimento': 'Jota', 'cnpj': '123456789', 'TipodeEstabelecimento': 'Restaurante', 'senha': '1234'}]
+listaUsuarios = [{'usuario': 'biafarah123', 'email': 'biaafarah@gmail.com', 'senha': '1234', 'cpf': '12345645678901'}]
+listaPostsUsuario = [{'usuário': 'biafarah123', 'post': 'Esse é meu primeiro post'}]
+listaPostsEstabelecimento = [{'usuário': 'biafarah123', 'post': 'Esse é meu primeiro post'}]
+listaReviews = [{'usuario': 'biafarah123', 'estabelecimento': 'Jota', 'review': 'Gostei mt do restaurante'}]
+listaCupons = [{'Nomeestabelecimento':'Jota', 'duração(dias)': '15', 'data_inicio': '11/09/2023', 'valor(%)': '15',
+         'descricao': 'Cupom de desconto de 15% em qualquer produto da loja'}]
+listaNotas = [{'estabelecimento': 'Jota', 'TipodeEstabelecimento': 'Restaurante', 'nota': '10'}]
 
-cupom = {'Nomeestabelecimento':'Jota', 'duração(dias)': '15', 'data_inicio': '11/09/2023', 'valor(%)': '15',
-         'descricao': 'Cupom de desconto de 15% em qualquer produto da loja'}
-estabelecimento = {'email':'oiii', 'estabelecimento': 'Jota', 'cnpj': '123456789', 'TipodeEstabelecimento': 'Restaurante', 'senha': '1234'}
-reviews = {'usuario': 'biafarah123', 'estabelecimento': 'Jota', 'review': 'Gostei mt do restaurante'}
-usuario = {'usuario': 'biafarah123', 'email': 'biaafarah@gmail.com', 'senha': '1234', 'cpf': '12345645678901'}
-post = {'usuário': 'biafarah123', 'post': 'Esse é meu primeiro post'}
-nota = {'estabelecimento': 'Jota', 'TipodeEstabelecimento': 'Restaurante', 'nota': '10'}
 
 'Menu Estabelecimento'
 def CadastrarCupom(listaCupons):
@@ -37,19 +31,90 @@ def CadastrarCupom(listaCupons):
             break
 
 def VerReviwsEstabelecimento():
-    return 2
+    reviews_estabelecimento = []
+    nomeestabelecimento = input('Qual é o nome do seu estabelecimento? ')
+    for review in listaReviews:
+        if review['estabelecimento'] == nomeestabelecimento:
+            reviews_estabelecimento.append(review)
 
-def FazerPostEstabelecimento():
-    return 3
+    if not reviews_estabelecimento:
+        print('Nenhuma review encontrada para este estabelecimento.')
+    else:
+        print('Reviews do estabelecimento:')
+        for review in reviews_estabelecimento:
+            print(f'Usuário: {review["usuario"]}')
+            print(f'Review: {review["review"]}')
+            print('-' * 20)
+
+
+def FazerPostEstabelecimento(listaPostsEstabelecimento):
+    while True:
+        postEstabelecimento = input('Escreva seu post: ')
+        if any(user['post'] == postEstabelecimento for user in listaPostsEstabelecimento):
+            print('Post já cadastrado')
+        else:
+            postEstabelecimento = {'usuário': 'biafarah123',
+                    'post': postEstabelecimento}
+            
+            listaPostsEstabelecimento.append(postEstabelecimento)
+            print('Post cadastrado com sucesso')
+            break
 
 
 'Menu usuários'
+def MenuUsuario():
+    menuUsuario = int(input('Seja bem vindo de volta ao Inclui+! \n Escolha uma opção: \n 1 - Fazer nova review \n 2 - ver suas reviews \n 3 - fazer um novo post na comunidade \n 4 - ver seus posts na comunidade \n 5 - dar nota para um estabelecimento: '))
+    if menuUsuario == 1:
+        FazerNovaReview(listaReviews)
+    elif menuUsuario == 2:
+        VerReviewsUsuario(listaReviews)
+    elif menuUsuario == 3:
+        FazerPostUsuario(listaPostsUsuario)
+    elif menuUsuario == 4:
+        VerPostsUsuario(listaPostsUsuario)
+    elif menuUsuario == 5:
+        DarNota(listaNotas)
+
+def menuEstabelecimento():
+    menuEstabelecimento = int(input('Seja bem vindo de volta ao Inclui+! \n Escolha uma opção: \n 1 - Cadastrar cupom \n 2 - Ver reviews do seu estabelecimento \n 3 - Fazer um novo post na comunidade:'))
+    if menuEstabelecimento == 1:
+        CadastrarCupom(listaCupons)
+    elif menuEstabelecimento == 2:
+        VerReviwsEstabelecimento()
+    elif menuEstabelecimento == 3:
+        FazerPostEstabelecimento(listaPostsEstabelecimento)
+
 def FazerNovaReview():
-    return 1
-1
+    while True:
+        nomeEstabelecimentoReview = input('Qual é o nome do estabelecimento que você deseja fazer uma review? ')
+        if any(user['estabelecimento'] == nomeEstabelecimentoReview for user in listaReviews):
+            print('Review já cadastrada')
+        else:
+            review = input('Escreva sua review: ')
+            reviewUsuario = {'usuario': 'biafarah123',
+                    'estabelecimento': nomeEstabelecimentoReview,
+                    'review': review}
+            
+            listaReviews.append(reviewUsuario)
+            print('Review cadastrada com sucesso')
+            break
 
 def VerReviewsUsuario():
-    return 2
+    reviews_usuario = []
+    login_usuario = input('Qual é seu nome de usuário? ')
+    for review in listaReviews:
+        if review['usuario'] == login_usuario:
+            reviews_usuario.append(review)
+
+    if not reviews_usuario:
+        print('Nenhuma review encontrada para este estabelecimento.')
+    else:
+        print('Reviews do estabelecimento:')
+        for review in reviews_usuario:
+            print(f'Usuário: {review["usuario"]}')
+            print(f'Estabelecimento: {review["estabelecimento"]}')
+            print(f'Review: {review["review"]}')
+            print('-' * 20)
 
 def FazerPostUsuario(listaPostsUsuario):
     while True:
@@ -64,8 +129,22 @@ def FazerPostUsuario(listaPostsUsuario):
             print('Post cadastrado com sucesso')
             break
 
-def VerPostsUsuario():
-    return 4
+def VerPostsUsuario(listaPostsUsuario):
+    posts_usuario = []
+    login_usuario = input('Qual é seu nome de usuário? ')
+    for post in listaPostsUsuario:
+        if post['usuario'] == login_usuario:
+            posts_usuario.append(post)
+
+    if not posts_usuario:
+        print('Nenhum post encontrado. ')
+    else:
+        print('Posts do estabelecimento:')
+        for post in posts_usuario:
+            print(f'Usuário: {post["usuario"]}')
+            print(f'Estabelecimento: {post["estabelecimento"]}')
+            print(f'Post: {post["post"]}')
+            print('-' * 20)
 
 def DarNota(listaNotas):
     while True:
@@ -122,27 +201,35 @@ def CadastraUsuario(listausuarios):
                 print('Cadastro realizado com sucesso')
                 break
 
-def LoginEstabelecimento():
-    menuEstabelecimento = int(input())
-    if menuEstabelecimento == 1:
-        CadastrarCupom(listaCupons)
-    elif menuEstabelecimento == 2:
-        VerReviwsEstabelecimento(listaReviews)
-    elif menuEstabelecimento == 3:
-        FazerPostEstabelecimento(listaPostsEstabelecimento)
+def LoginEstabelecimento(*args):
+    emailestabelecimento = input('Bem vindo de volta a Inclui+! Qual é seu email? ')
+    if any(user['email'] == emailestabelecimento for user in listaEstabelecimentos):
+        senhaEstabelecimento = input('Qual é sua senha?: ')
+        if any(user['senha'] == senhaEstabelecimento for user in listaEstabelecimentos):
+            print('Login realizado com sucesso')
+            menuEstabelecimento()
+        else:
+            print('Senha incorreta')
+            LoginEstabelecimento(listaEstabelecimentos)
+    else:
+        print('Email não cadastrado')
+        CadastraEstabelecimento(listaEstabelecimentos)
 
-def LoginUsuario():
-    menuUsuario = int(input('Seja bem vindo ao Inclui+! \n Escolha uma opção: \n 1 - cadastrar estabelecimento \n 2 - cadastrar usuário \n 3 - login estabelecimento \n 4 - login usuário: '))
-    if menuUsuario == 1:
-        FazerNovaReview(listaReviews)
-    elif menuUsuario == 2:
-        VerReviewsUsuario(listaReviews)
-    elif menuUsuario == 3:
-        FazerPostUsuario(listaPostsUsuario)
-    elif menuUsuario == 4:
-        VerPostsUsuario(listaPostsUsuario)
-    elif menuUsuario == 5:
-        DarNota(listaNotas)
+def LoginUsuario(listaUsuarios):
+    emailusuario = input('Bem vindo de volta a Inclui+! Qual é seu email? ')
+    if any(user['email'] == emailusuario for user in listaUsuarios):
+        senhaUsuario = input('Qual é sua senha?: ')
+        if any(user['senha'] == senhaUsuario for user in listaUsuarios):
+            print('Login realizado com sucesso')
+            MenuUsuario()
+        else:
+            print('Senha incorreta')
+            LoginUsuario(listaUsuarios)
+    else:
+        print('Email não cadastrado')
+        CadastraUsuario(listaUsuarios)
+
+
 
 menu = int(input('Seja bem vindo ao Inclui+! \n Escolha uma opção: \n 1 - cadastrar estabelecimento \n 2 - cadastrar usuário \n 3 - login estabelecimento \n 4 - login usuário: '))
 if menu == 1:
@@ -152,5 +239,5 @@ elif menu == 2:
 elif menu == 3:
     LoginEstabelecimento()
 elif menu == 4:
-    LoginUsuario()
+    LoginUsuario(listaUsuarios)
 
